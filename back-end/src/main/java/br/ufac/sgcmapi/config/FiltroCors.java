42cbca8cae7529c6df992fe59efea45fa81786c1
Adegilson <adegilsongs2013@gmail.com>
2022-11-21 18:01:38 -0500

    url

--- back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ back-end/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -18,8 +18,7 @@ public class FiltroCors {
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedOrigins(
             Arrays.asList(
-                "http://127.0.0.1:5500",
-                "https://localhost:4200")
+                "https://sgcm-adegilson-front-production.up.railway.app")
         );
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
